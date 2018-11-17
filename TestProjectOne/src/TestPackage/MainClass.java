041c893b49c77ef55638e19e9bf33b6702d817aa
vbstudent <vbstudent@CL3-16.uit.edu>
2018-11-17 11:00:35 +0500

    Change id:SamTestChangeId001

---
+++ TestProjectOne/src/TestPackage/MainClass.java
@@ -0,0 +1,56 @@
+package TestPackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\vbstudent\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+				
+		driver.get("https://demo.opencart.com/index.php?route=account/login");	
+		driver.manage().window().maximize();
+
+
+		 WebElement username;
+		 username = driver.findElement(By.id("input-email"));
+		 username.sendKeys("mytc5@gmail.com");
+
+		 WebElement Password;
+		 Password = driver.findElement(By.id("input-password"));
+		 Password.sendKeys("123456789");
+
+		 WebElement loginButton;  
+		 loginButton = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[2]/div/form/input"));
+		 loginButton.click(); 
+
+		 String actualpagetitle = driver.getTitle();
+		 String expectedTitle = "My Account";
+		 
+		 if (actualpagetitle.contentEquals(expectedTitle)){
+	            System.out.println("Test Passed!");
+	        } else {
+	            System.out.println("Test Failed");
+	        }
+		
+		driver.close();
+		
+		driver.quit();
+		
+	
+	
+	
+	
+	}
+
+	
+	
+
+}
